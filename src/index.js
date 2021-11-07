beea892bcfa69cbb531298654b0482b7ecd43763
Allusive <auraud.zarafshar@gmail.com>
2021-11-07 14:38:59 -0800

    Simple POC work for only mod users

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
+const tmi = require("tmi.js");
 const OBSWebSocket = require("obs-websocket-js");
 var obs = new OBSWebSocket();
+
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    reconnect: true,
+  },
+
+  channels: ["allusve"],
+});
+client.connect().catch((err) => {
+  console.log(err);
+});
+
+client.on("chat", (channel, user, message, self) => {
+  console.log(`${user["display-name"]}: ${message}`);
+  if (user.mod) {
+    // User is a mod.
+    if (message.startsWith("!brb")) {
+      setScene("BRB");
+    }
+  }
+});
 
 function setScene(scene) {
   obs
     .connect({
       address: process.env.OBS_WEBSOCKET_IP,
-      password: process.env.OBS_PASSWORD,
+      password: process.env.OBS_WEBSOCKET_PASSWORD,
     })
     .then(() => {
       obs.send("SetCurrentScene", {
@@ -15,11 +38,12 @@ function setScene(scene) {
     .then((data) => {
       console.log(`Set current scene to ${scene}`);
     })
+    .then(() => {
+      obs.on("error", (err) => {
+        console.error("socket error:", err);
+      });
+    })
     .catch((err) => {
       console.error("Seting Sene Failed", err);
     });
 }
-obs.on("error", (err) => {
-  console.error("socket error:", err);
-});
-setScene("BRB");
